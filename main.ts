ff8e82eaea7c621320c47fd5c1112005d3b0cb8d
yuliia555 <113055649+yuliia555@users.noreply.github.com>
2022-09-19 20:13:30 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,29 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendString(Hello)
-})
-input.onButtonPressed(Button.AB, function () {
-	
-})
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == Hello) {
-        basic.showLeds(`
-            . . # . .
-            # . # . #
-            . # # # .
-            # . # . #
-            . . # . .
-            `)
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+    basic.pause(100)
+    basic.showNumber(num)
+    basic.pause(100)
+    if (receivedNumber == num) {
+        basic.showString(" Yes!")
+    } else {
+        basic.showString(" No(")
     }
 })
-input.onButtonPressed(Button.B, function () {
-	
+input.onButtonPressed(Button.A, function () {
+    num += 1
+    basic.showNumber(num)
+    num = num
 })
-let Hello = ""
-Hello = "Hello!"
+input.onButtonPressed(Button.B, function () {
+    num += -1
+    basic.showNumber(num)
+    num = num
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(num)
+    num = num
+})
+let num = 0
+num = 0
 radio.setGroup(55)
+basic.showNumber(num)
